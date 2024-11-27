aef1d435c48172ac4e27173d367c76778c59f9ed
yudhakurniawan <yudhakurniawan21@gmail.com>
2024-11-27 08:30:00 +0700

    change middleware

--- middleware.ts
+++ middleware.ts
@@ -4,50 +4,61 @@ import { auth } from "@/auth";
 export default auth((req) => {
   const { nextUrl, auth: session } = req;
 
-  // Aturan akses dinamis
+  console.log("Middleware triggered for:", nextUrl.pathname);
+  console.log("Session:", session);
+
   const routes = [
     {
       path: "/dashboard",
-      requiresAuth: true, // Halaman memerlukan autentikasi
-      redirectIfUnauthenticated: "/login", // Redirect jika tidak login
+      requiresAuth: true,
+      redirectIfUnauthenticated: "/login",
     },
     {
       path: "/login",
-      requiresAuth: false, // Halaman tidak memerlukan autentikasi
-      redirectIfAuthenticated: "/", // Redirect jika sudah login
+      requiresAuth: false,
+      redirectIfAuthenticated: "/",
     },
     {
       path: "/signup",
       requiresAuth: false,
-      redirectIfAuthenticated: "/", // Redirect jika sudah login
+      redirectIfAuthenticated: "/",
     },
   ];
 
-  // Cari aturan untuk halaman yang diminta
   const route = routes.find((r) => nextUrl.pathname.startsWith(r.path));
 
   if (route) {
     const isLoggedIn = !!session;
+    console.log("Route found:", route);
 
-    // Redirect jika pengguna tidak login dan halaman memerlukan autentikasi
     if (!isLoggedIn && route.requiresAuth) {
+      console.log("Redirecting to:", route.redirectIfUnauthenticated);
       return NextResponse.redirect(
         new URL(route.redirectIfUnauthenticated ?? "", nextUrl.origin)
       );
     }
 
-    // Redirect jika pengguna sudah login dan halaman tidak memerlukan autentikasi
     if (isLoggedIn && route.redirectIfAuthenticated) {
+      console.log("Redirecting to:", route.redirectIfAuthenticated);
       return NextResponse.redirect(
         new URL(route.redirectIfAuthenticated, nextUrl.origin)
       );
     }
   }
 
-  // Untuk semua kasus lainnya, izinkan permintaan
+  console.log("No rules matched, proceeding to next response.");
   return NextResponse.next();
 });
 
 export const config = {
-  matcher: ["/login", "/signup", "/dashboard", "/"], // Tentukan URL yang akan diperiksa
+  matcher: [
+    /*
+     * Match all request paths except for the ones starting with:
+     * - api (API routes)
+     * - _next/static (static files)
+     * - _next/image (image optimization files)
+     * - favicon.ico, sitemap.xml, robots.txt (metadata files)
+     */
+    "/((?!api|_next/static|_next/image|favicon.ico|sitemap.xml|robots.txt).*)",
+  ],
 };
